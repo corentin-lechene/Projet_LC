3f1dee4764a83ac76049ab1d35963aab58ac0d51
Corentin <corentin.lechene.pro@gmail.com>
2022-02-24 08:07:47 +0100

    Test API - sauvegarde

--- server/app/config/db.config.js
+++ server/app/config/db.config.js
@@ -1,8 +1,8 @@
 module.exports = {
-    HOST: "localhost",
+    HOST: "35.233.27.141",
     USER: "root",
-    PASSWORD: "root",
-    DB: "projetlc",
+    PASSWORD: "us8ypgCyo9pFvlA8",
+    DB: "projetlc-test",
     dialect: "mysql",
     pool: {
         max: 5,
@@ -11,3 +11,17 @@ module.exports = {
         idle: 10000
     }
 };
+//
+// module.exports = {
+//     HOST: "localhost",
+//     USER: "root",
+//     PASSWORD: "root",
+//     DB: "projetlc",
+//     dialect: "mysql",
+//     pool: {
+//         max: 5,
+//         min: 0,
+//         acquire: 30000,
+//         idle: 10000
+//     }
+// };
