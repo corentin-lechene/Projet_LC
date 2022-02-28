dba9be829893cf34d846ab1a288f87f9eb2cd4ac
Hadrien <hsuryous@gmail.com>
2022-02-28 17:03:11 +0100

    Change form + add modify info on profile

--- src/router/views/profile/profile.vue
+++ src/router/views/profile/profile.vue
@@ -140,6 +140,64 @@ export default {
                   <span class="d-none d-md-inline-block" style="margin-top: 10%;">Mes informations</span>
                 </template>
                 <div class="row">
+                  <div class="col-2"></div>
+                  <div class="col-10">
+                    <div class="row" style="width: 100%;">
+                      <div class="col-6">
+                        <label>Nom</label>
+                        <div class="row" style="height: 5px; width: 100%;"></div>
+                      </div>
+                      <div class="col-6">
+                        <label>Prénom</label>
+                        <div class="row" style="height: 5px; width: 100%;"></div>
+                      </div>
+                    </div>
+                    <div class="row" style="width: 100%;">
+                      <div class="col-6">
+                        <b-form-input id="input-default" style="width: 80%; text-align: center;" placeholder="Hkfeo"></b-form-input>
+                      </div>
+                      <div class="col-6">
+                        <b-form-input id="input-default" style="width: 80%; text-align: center;" placeholder="Sarah"></b-form-input>
+                      </div>
+                    </div>
+                    <div class="row" style="height: 35px; width: 100%;"></div>
+                    <div class="row" style="width: 100%;">
+                      <div class="col-6">Téléphone
+                        <div class="row" style="height: 5px; width: 100%;"></div>
+                      </div>
+                      <div class="col-6">Email
+                        <div class="row" style="height: 5px; width: 100%;"></div>
+                      </div>
+                    </div>
+                    <div class="row" style="width: 100%;">
+                      <div class="col-6">
+                        <b-form-input id="input-default" style="width: 80%; text-align: center;" placeholder="0758585858"></b-form-input>
+                      </div>
+                      <div class="col-6">
+                        <b-form-input id="input-default" style="width: 80%; text-align: center;" placeholder="michel@gmail.com"></b-form-input>
+                      </div>
+                    </div>
+                    <div class="row" style="height: 35px; width: 100%;"></div>
+                    <div class="row" style="width: 100%;">
+                      <div class="col-6">Adresse
+                        <div class="row" style="height: 5px; width: 100%;"></div>
+                      </div>
+                      <div class="col-6">Code postal
+                        <div class="row" style="height: 5px; width: 100%;"></div>
+                      </div>
+                    </div>
+                    <div class="row" style="width: 100%;">
+                      <div class="col-6">
+                        <b-form-input id="input-default" style="width: 80%; text-align: center;" placeholder="12 avenue de la liberté"></b-form-input>
+                      </div>
+                      <div class="col-6">
+                        <b-form-input id="input-default" style="width: 80%; text-align: center;" placeholder="92250"></b-form-input>
+                      </div>
+                    </div>
+                    <div class="row" style="height: 35px; width: 100%;"></div>
+                  </div>
+                </div>
+                <div class="row">
                   <b-modal id="modal-1" title="Modifier mes informations" hide-footer size="lg" centered>
                     <div class="col-12">
                       <div class="card">
